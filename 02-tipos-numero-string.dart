c715a59eb736b2475d83801b8e9914aa91bbd7f8
Alfredo Sánchez Verduzco <AlfredoSanchezVerduzco@outlook.com>
2023-02-19 08:54:36 -0600

    Se agrega código a archivo 02-tipos-numero-string.dart

---
+++ 02-tipos-numero-string.dart
@@ -0,0 +1,28 @@
+void main() {
+ 
+  
+  //var nombre = "Alfredo";
+  //var apellido = "Sanchez";
+  
+  //final String nombre = "Alfredo";
+  //final nombre = "Alfredo";
+  //const String nombre = "Alfredo"; //No cambia valor en tiempo de compilación
+    
+  String nombre = "Alfredo";
+  String apellido = "Sanchez";
+  
+  print('$nombre $apellido');
+  
+  
+  // Nuúmero
+  
+  int empleados = 100;
+  
+  double salario = 12000.87;
+  
+  print(empleados);
+  
+  print(salario);
+  
+  
+}
