42ebc294cb623b0830e3317d10c5c2840672406d
Alfredo Sánchez Verduzco <AlfredoSanchezVerduzco@outlook.com>
2023-02-26 20:38:34 -0600

    Se agrega 05-map.dart

---
+++ 05-map.dart
@@ -0,0 +1,23 @@
+void main() {
+  
+  /*Map persona = {
+    
+    'nombre' : 'Alfredo',
+    'edad' : 35,
+    'soltero': true
+    
+    
+  };*/
+  
+  Map<String,dynamic> persona = {
+    
+    'nombre' : 'Alfredo',
+    'edad' : 35,
+    'soltero': true
+    
+    
+  };
+  
+  print( persona );
+  
+}
